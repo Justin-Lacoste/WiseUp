f345704e79f803eff6b74212c4aef082d708441b
henri123lemoine <henri123lemoine@gmail.com>
2023-01-29 05:06:31 -0500

    updated app.py

--- src/app.py
+++ src/app.py
@@ -1,22 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 from extract import Extract
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-
+    return "Hello, World!"
 
 # Extracts text and embeddings from tuple (type of file, raw file data)
-@app.route("/extract/<file_type>/<file_data>")
-def extract(file_type, file_data):
-    data = request.form['input_name']
-
-    extract = Extract()
-    if file_type == "pdf":
-        pages_text = extract.pdf2text(file_data)
+@app.route("/extract/", methods=['GET', 'POST'])
+def extract():#file_type, file_data):
+    data = request.form["file"]
+    print(data, type(data))
+    
+    # extract = Extract()
+    # if file_type == "pdf":
+    #     pages_text = extract.pdf2text(file_data)
+    
+    # print(pages_text)
+    pass
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80)
